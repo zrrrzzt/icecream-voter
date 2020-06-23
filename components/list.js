e6a373698561820369f4387e294a9a228f18610f
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-06-23 21:19:35 +0200

    Adds first icecream

---
+++ components/list.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+const ListItem = props => {
+  const { id, name, producer, image } = props
+  return (
+    <Link href={`/${id}`}>
+      <a className='cursor-pointer'>
+        <div className='flex h-48'>
+          <div>
+            <img src={`images/${image}`} className='h-48' />
+          </div>
+          <div className='flex flex-col justify-center h-full'>
+            <h2 className='text-3xl'>{name}</h2>
+            <div>{producer}</div>
+          </div>
+        </div>
+      </a>
+    </Link>
+  )
+}
+
+export default ListItem
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import ListItem from '../components/list'
+const icecreams = require('../lib/data/icescreams.json')
+
 const HomePage = () => {
   return (
     <div className='flex flex-col items-center'>
-      <h1 className='text-4xl font-mono'>Velkommen</h1>
+      <h1 className='text-4xl font-mono'>PAWs istest 2020</h1>
+      { icecreams.map(ListItem) }
     </div>
   )
 }
