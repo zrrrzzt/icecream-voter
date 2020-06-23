a3c90580dcf73b581fc077ec90521b0936fd9736
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-06-23 20:39:14 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className='flex flex-col items-center'>
+      <h1 className='text-4xl font-mono'>Velkommen</h1>
+    </div>
+  )
+}
+
+export default HomePage
