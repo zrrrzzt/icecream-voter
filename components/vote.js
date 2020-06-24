ae16a6d1de5b47844a6369d02cdf5c985b5530da
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-06-24 16:33:55 +0200

    Adds more options (patch)

--- components/vote.js
+++ components/vote.js
@@ -4,24 +4,24 @@ const VoteCard = () => {
       <div className='px-6 py-4'>
         <div className='font-bold text-xl mb-2'>Gi din stemme</div>
         <div>
-          <label for='smak'>Smak</label>
+          <label htmlFor='smak'>Smak</label>
           <input type='range' min='0' max='100' className='w-full' name='smak' id='smak' />
         </div>
         <div>
-          <label for='smak'>Smak</label>
-          <input type='range' min='0' max='100' className='w-full' name='smak' id='smak' />
+          <label htmlFor='trekk'>Trekk</label>
+          <input type='range' min='0' max='100' className='w-full' name='trekk' id='trekk' />
         </div>
         <div>
-          <label for='smak'>Smak</label>
-          <input type='range' min='0' max='100' className='w-full' name='smak' id='smak' />
+          <label htmlFor='holdbarhet'>Holdbarhet i varmen</label>
+          <input type='range' min='0' max='100' className='w-full' name='holdbarhet' id='holdbarhet' />
         </div>
         <div>
-          <label for='smak'>Smak</label>
-          <input type='range' min='0' max='100' className='w-full' name='smak' id='smak' />
+          <label htmlFor='helhet'>Helhetsopplevelse</label>
+          <input type='range' min='0' max='100' className='w-full' name='helhet' id='helhet' />
         </div>
         <div>
-          <label for='smak'>Smak</label>
-          <input type='range' min='0' max='100' className='w-full' name='smak' id='smak' />
+          <label htmlFor='anbefalt'>Anbefalt til en venn?</label>
+          <input type='range' min='0' max='100' className='w-full' name='anbefalt' id='anbefalt' />
         </div>
         <div className='flex justify-end'>
           <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
