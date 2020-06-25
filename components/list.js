d02d1e28feefb5a8882ba69b7f31b459b1e78128
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-06-25 18:38:39 +0200

    Adds login to frontpage as well (patch)

--- components/list.js
+++ components/list.js
@@ -5,16 +5,16 @@ const ListItem = props => {
   return (
     <Link href={`/${id}`} key={id}>
       <a className='cursor-pointer'>
-        <div class='max-w-sm rounded overflow-hidden shadow-lg'>
-          <img class='w-full' src={`images/${image}`} alt={name} />
-          <div class='px-6 py-4'>
-            <div class='font-bold text-xl mb-2'>{name}</div>
-            <p class='text-gray-700 text-base'>
+        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+          <img className='w-full' src={`images/${image}`} alt={name} />
+          <div className='px-6 py-4'>
+            <div className='font-bold text-xl mb-2'>{name}</div>
+            <p className='text-gray-700 text-base'>
               {producer}
             </p>
           </div>
-          <div class='flex justify-end px-6 py-4'>
-            <span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>68/100</span>
+          <div className='flex justify-end px-6 py-4'>
+            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>Vis detaljer</span>
           </div>
         </div>
       </a>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import { useSession } from 'next-auth/client'
 import ListItem from '../components/list'
+import LoginButton from '../components/login-button'
+import LoggedInCard from '../components/logged-in-card'
 const icecreams = require('../lib/data/icescreams.json')
 
 const HomePage = () => {
+  const [session, loading] = useSession()
   return (
     <div className='flex flex-col items-center'>
       <h1 className='text-4xl font-mono'>PAWs istest 2020</h1>
       {icecreams.map(ListItem)}
+      {!session && <LoginButton />}
+      {session && <LoggedInCard user={session.user} />}
     </div>
   )
 }
