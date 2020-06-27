6cfc1e090fc12df06e8500774d7672fa9e52bb8c
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-06-27 09:08:40 +0200

    Visual updates to highest vote (patch)

--- components/highest-vote.js
+++ components/highest-vote.js
@@ -3,13 +3,14 @@ export default props => {
   return (
     <div className='max-w-sm w-full rounded overflow-hidden shadow-lg'>
       <div className='px-6 py-4'>
-        <div className='font-bold text-xl mb-2'>Best likt av</div>
-        <div>
-          <img src={image} />
-        </div>
         <div className='flex justify-between'>
-          <span>{name.split('.')[0]}</span>
-          <span className='font-bold'>{Math.floor(total / 5)}/100</span>
+          <div>
+            <img src={image} className='w-24' />
+          </div>
+          <div className='flex flex-col'>
+            <span className='text-right'>Best likt av {name.split('.')[0]}</span>
+            <span className='font-bold text-right'>{Math.floor(total / 5)}/100 poeng</span>
+          </div>
         </div>
       </div>
     </div>
