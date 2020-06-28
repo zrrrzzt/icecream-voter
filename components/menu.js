20c8abfc9b22acf253bb100b3b26350168c53d1c
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-06-28 19:05:23 +0200

    Changes text for Login (patch)

--- components/menu.js
+++ components/menu.js
@@ -18,7 +18,7 @@ export default () => {
   const LoginButton = () => {
     return (
       <button onClick={handleLogin} className='bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease'>
-        Logg inn for å stemme
+        Logg inn
       </button>
     )
   }
