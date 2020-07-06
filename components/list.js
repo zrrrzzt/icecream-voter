dad32e4d7a017e9d08c3704d217eb7419064b9e0
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-07-06 19:14:17 +0200

    Adds 0 for no votes (patch)

--- components/list.js
+++ components/list.js
@@ -3,25 +3,14 @@ import { useEffect, useState } from 'react'
 import * as FirestoreService from '../lib/firestore-service'
 import calculateScore from '../lib/calculate-score'
 
-const Score = props => {
-  const { score } = props
-  return (
-    <span className='inline-block bg-gray-200 rounded-full px-3 py-1 mr-2 text-sm font-semibold text-gray-700'>
-      {score}/100
-    </span>
-  )
-}
-
 const ListItem = props => {
   const { id, name, producer, image } = props
-  const [score, setScore] = useState(false)
+  const [score, setScore] = useState(0)
 
   useEffect(() => {
     FirestoreService.getVotes(id).then(vote => {
       const docs = vote.docs.map(doc => doc.data())
-      if (docs.length > 0) {
-        setScore(calculateScore(docs))
-      }
+      setScore(calculateScore(docs))
     })
   }, [id, setScore])
 
@@ -37,7 +26,7 @@ const ListItem = props => {
             </p>
           </div>
           <div className='flex justify-end px-6 py-4'>
-            {score && <Score score={score} />}
+            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 mr-2 text-sm font-semibold text-gray-700'>{score}/100</span>
             <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>Vis detaljer</span>
           </div>
         </div>
