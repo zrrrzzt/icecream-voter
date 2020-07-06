8bb2059e6aa1693b088fff1477c9bd7dd493f610
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-07-06 19:33:42 +0200

    Adds sorting after score (patch)

--- components/list.js
+++ components/list.js
@@ -15,23 +15,25 @@ const ListItem = props => {
   }, [id, setScore])
 
   return (
-    <Link href={`/${id}`} key={id}>
-      <a className='cursor-pointer'>
-        <div className='max-w-sm rounded overflow-hidden shadow-lg mb-2'>
-          <img className='w-full' src={`images/${image}`} alt={name} />
-          <div className='px-6 py-4'>
-            <div className='font-bold text-xl mb-2'>{name}</div>
-            <p className='text-gray-700 text-base'>
-              {producer}
-            </p>
+    <div className={`order-${100 - score}`} key={id}>
+      <Link href={`/${id}`}>
+        <a className='cursor-pointer'>
+          <div className='max-w-sm rounded overflow-hidden shadow-lg mb-2'>
+            <img className='w-full' src={`images/${image}`} alt={name} />
+            <div className='px-6 py-4'>
+              <div className='font-bold text-xl mb-2'>{name}</div>
+              <p className='text-gray-700 text-base'>
+                {producer}
+              </p>
+            </div>
+            <div className='flex justify-end px-6 py-4'>
+              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 mr-2 text-sm font-semibold text-gray-700'>{score}/100</span>
+              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>Vis detaljer</span>
+            </div>
           </div>
-          <div className='flex justify-end px-6 py-4'>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 mr-2 text-sm font-semibold text-gray-700'>{score}/100</span>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>Vis detaljer</span>
-          </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </div>
   )
 }
 
