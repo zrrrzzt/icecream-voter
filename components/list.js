78fa7ebe9ac8c7f9d6576929096522796dc309a4
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-07-07 22:28:08 +0200

    Adds fetch data for frontpage (patch)

--- components/list.js
+++ components/list.js
@@ -1,39 +1,26 @@
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import * as FirestoreService from '../lib/firestore-service'
-import calculateScore from '../lib/calculate-score'
 
 const ListItem = props => {
-  const { id, name, producer, image } = props
-  const [score, setScore] = useState(0)
-
-  useEffect(() => {
-    FirestoreService.getVotes(id).then(vote => {
-      const docs = vote.docs.map(doc => doc.data())
-      setScore(calculateScore(docs))
-    })
-  }, [id, setScore])
-
+  const { id, name, producer, image, score } = props
+  
   return (
-    <div className={`order-${100 - score}`} key={id}>
-      <Link href={`/${id}`}>
-        <a className='cursor-pointer'>
-          <div className='max-w-sm rounded overflow-hidden shadow-lg mb-2'>
-            <img className='w-full' src={`images/${image}`} alt={name} />
-            <div className='px-6 py-4'>
-              <div className='font-bold text-xl mb-2'>{name}</div>
-              <p className='text-gray-700 text-base'>
-                {producer}
-              </p>
-            </div>
-            <div className='flex justify-end px-6 py-4'>
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 mr-2 text-sm font-semibold text-gray-700'>{score}/100</span>
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>Vis detaljer</span>
-            </div>
+    <Link href={`/${id}`}>
+      <a className='cursor-pointer'>
+        <div className='max-w-sm rounded overflow-hidden shadow-lg mb-2'>
+          <img className='w-full' src={`images/${image}`} alt={name} />
+          <div className='px-6 py-4'>
+            <div className='font-bold text-xl mb-2'>{name}</div>
+            <p className='text-gray-700 text-base'>
+              {producer}
+            </p>
           </div>
-        </a>
-      </Link>
-    </div>
+          <div className='flex justify-end px-6 py-4'>
+            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 mr-2 text-sm font-semibold text-gray-700'>{score}/100</span>
+            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>Vis detaljer</span>
+          </div>
+        </div>
+      </a>
+    </Link>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react'
+import * as FirestoreService from '../lib/firestore-service'
 import ListItem from '../components/list'
+import calculateScore from '../lib/calculate-score'
 const icecreams = require('../lib/data/icescreams.json')
 
+function scoreSort (b, a) {
+  return a.score - b.score
+}
+
 const HomePage = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    (async () => {
+      const jobs = icecreams.map(icecream => FirestoreService.getVotes(icecream.id))
+      const results = await Promise.all(jobs)
+      const allVotes = results.map(result => result.docs.map(doc => doc.data()))
+      const scores = allVotes.map(vote => calculateScore(vote))
+      const icecreamData = icecreams.map((icecream, index) => Object.assign({}, icecream, { score: scores[index]}))
+      icecreamData.sort(scoreSort)
+      setData(icecreamData)
+    })()
+  }, [icecreams, setData])
   return (
     <>
       <div className='flex flex-col items-center'>
         <h1 className='text-4xl font-mono'>PAWs istest 2020</h1>
-        {icecreams.map(ListItem)}
+        {data.map(icecream => <ListItem {...icecream} key={icecream.id} />)}
       </div>
     </>
   )
